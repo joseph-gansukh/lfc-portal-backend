da391bb6019e5f63a75af05c4c92230c219cd134
joseph-gansukh <g.sukhbat@gmail.com>
2019-11-20 15:14:14 -0600

    updated session_store to be dynamic

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
+if Rails.env == "production"
 Rails.application.config.session_store :cookie_store, key: "_lfc_portal_authentication_", domain: "https://lfc-portal-api.herokuapp.com/"
+else 
+  Rails.application.config.session_store :cookie_store, key: "_lfc_portal_authentication_"
+end
