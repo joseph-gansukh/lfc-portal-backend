34a6beb93d190e74a98f1f2eca07e013a1a42176
joseph-gansukh <g.sukhbat@gmail.com>
2019-11-21 13:30:37 -0600

    updated cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do 
-    origins "https://lfc-portal.herokuapp.com/" 
+    origins "https://frozen-sea-82512.herokuapp.com/" 
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
